7a48423d252d21dfc6e1b4f0f4e7545b16094fd4
Bevilhonda <marcelinyuri@gmail.com>
2024-01-24 13:30:46 -0300

    organiza componente

--- src/Pages/SideMenu.tsx
+++ src/Pages/SideMenu.tsx
@@ -1,20 +1,18 @@
 import Paper from '@mui/material/Paper';
-import HomeIcon from '@mui/icons-material/Home';
-import { Link } from 'react-router-dom';
 import MenuOptions from '../Menu/MenuOptions';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
 
 
 function SideMenu() {
+
   const location = useLocation();
   const libraryDetails = location.state?.libraryDetails || null;
 
 
-
   return (
     <div>
-
-      <div>
+      <div style={{ marginTop: '-100px', textAlign: 'left' }}>
 
         <Link to="/">
 
@@ -22,67 +20,59 @@ function SideMenu() {
 
         </Link>
 
-
-        <h1 style={{
-          color: "black",
-          textAlign: "center",
-          fontSize: "60px",
-          fontFamily: "sans-serif",
-          marginTop: '-25px'
-        }}> Bibliotecas Maringá </h1>
-
       </div>
 
-
-      <Paper style={{
-        width: 210,
-        backgroundColor: 'black',
-        padding: 10,
-        boxSizing: 'border-box',
-        height: '450px',
-        marginTop: '-20px',
-        marginRight: '10px'
-      }}>
-        <MenuOptions />
-
-
-      </Paper>
       <div>
-        {libraryDetails && (
-          <div style={{
-            marginTop: '-450px',
-            marginLeft: '300px',
-            color: 'greenyellow',
-            backgroundColor: "black",
-            padding: '5px',
-            border: '2px solid #ddd',
-            borderRadius: '15px',
-            boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)',
-            boxSizing: 'border-box',
-            width: '550px',
-            height: "450px",
-            marginBottom: '15px',
-            marginRight: '-20px'
 
 
-          }}>
+        <Paper style={{
+          width: 210,
+          backgroundColor: 'black',
+          padding: 10,
+          boxSizing: 'border-box',
+          height: '450px',
+          marginTop: '130px',
+          marginRight: '10px',
+          border: '2px solid #ddd',
+          borderRadius: '10px',
+          boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)',
+
+        }}>
+          <MenuOptions />
+
+        </Paper>
+
+        <div>
+          {libraryDetails && (
+            <div style={{
+              marginTop: '-525px',
+              marginLeft: '-1px',
+              color: 'greenyellow',
+              backgroundColor: "black",
+              padding: '5px',
+              border: '2px solid #ddd',
+              borderRadius: '10px',
+              boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)',
+              boxSizing: 'border-box',
+              width: '212px',
+              height: "70px",
+              marginBottom: '10px',
+              marginRight: '-20px'
 
 
-            <h3 style={{ fontSize: '15px' }}>Nome da Biblioteca : {libraryDetails.nome}</h3>
-            <ul style={{ fontSize: "15px" }}>
-              <p>Endereço:</p>
-              <p>Rua: {libraryDetails.rua}</p>
-              <p>Número: {libraryDetails.numero}</p>
-              <p>Cidade: {libraryDetails.cidade}</p>
-              <p>Bairro: {libraryDetails.bairro}</p>
-              <p>Estado: {libraryDetails.estado}</p>
+            }}>
 
-            </ul>
+              <h3 style={{
+                fontSize: '15px',
+                marginTop: '-1px'
+              }}>Biblioteca selecionada: {
+              libraryDetails.nome}  </h3>
 
-          </div>
-        )}
+            </div>
+          )}
+        </div>
+
       </div>
-
     </div>
   )
 }
