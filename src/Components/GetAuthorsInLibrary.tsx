836d01945e5477a8442b4a4e70a15d19893b6ee4
Bevilhonda <marcelinyuri@gmail.com>
2024-01-24 13:21:44 -0300

    atualiza função

--- src/Components/GetAuthorsInLibrary.tsx
+++ src/Components/GetAuthorsInLibrary.tsx
@@ -1,52 +1,98 @@
 import axios from "axios";
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
-type ListAuthor = {
-  authors: {
-    nome:string ,
-    sobrenome : string 
-  }
-  
-   
-}
+type Author = {
+  nome: string;
+  sobrenome: string;
+};
 
-const GetAuthorsInLibrary = function(){
+type ListAuthorResponse = {
+  authors: Author[];
+};
 
-  const [listAuthors , setListAuthors] = useState <ListAuthor[] | null > (null);
 
-  const searchAuthorInTheLibrary = async function(){
-     try {
-      const response = await axios.get('http://localhost:8080/SearchAuthors/{id}');
+const GetAuthorsInLibrary = function () {
 
-      setListAuthors (response.data.authors)
-     }
-     catch (error) {
-      console.error('Erro na requisição:', error);
+  const location = useLocation();
+  const getIdLibrary = location.state?.libraryDetails || null;
+
+  const idLibrary = getIdLibrary.id_biblioteca;
+
+  const [listAuthors, setListAuthors] = useState<Author[] | null>(null);
+
+  useEffect(() => {
+
+    const searchAuthorInTheLibrary = async function () {
+      try {
+
+        const response = await axios.get<ListAuthorResponse>(`http://localhost:8080/SearchAuthors/${idLibrary}`);
+
+        setListAuthors(response.data.authors)
+
+      }
+      catch (error) {
+        console.error('Erro na requisição:', error);
+      }
+    };
+
+    if (idLibrary !== null && idLibrary !== undefined) {
+      searchAuthorInTheLibrary();
     }
-  };
 
-  useEffect( ()=>{
-    searchAuthorInTheLibrary();
-
-  },[]);
+  }, [idLibrary]);
 
   return (
-    <div>
+    <div style={{
+
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+      width: 900,
+      padding: 10,
+      marginTop: '-550px',
+      marginLeft: '250px',
+    }}>
       {listAuthors ? (
-        listAuthors.map((author : ListAuthor) => (
-          <div key={author.authors?.toString()}> 
-          <h3>
-            {`${author.authors.nome} ${author.authors.sobrenome}`}
-            
-          </h3>
-      
+        listAuthors.map((author: Author, index) => (
+          <div key={index} style={{
+            color: 'greenyellow',
+            backgroundColor: "black",
+            padding: '8px',
+            border: '2px solid #ddd',
+            borderRadius: '15px',
+            boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)',
+            boxSizing: 'border-box',
+            width: '175px',
+            height: "110px",
+            marginBottom: '15px',
+            marginRight: '300px'
+
+          }}>
+            <div style={{ fontSize: '13px' }}>
+
+              <div style={{ marginTop: '2px' }}>
+                <strong>Nome:</strong> {author.nome}
+              </div>
+
+              <div>
+                <strong>Sobrenome:</strong> {author.sobrenome}
+              </div>
+
+            </div>
+
           </div>
 
         ))
-        ) : (
-          <p style={{ color: 'greenyellow', fontSize: '18px' }}>Carregando Autores...</p>
-        )}
-      
+      ) : (
+        <p style={{
+          color: 'greenyellow',
+          fontSize: '18px',
+
+
+        }}>Carregando Autores...</p>
+      )}
+
     </div>
   );
 }
