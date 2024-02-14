fbdf13941fdd669981c6e0be3f9c047bbc0a669f
Bevilhonda <marcelinyuri@gmail.com>
2024-02-14 15:01:21 -0300

    cria função para rotas de renderização

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,27 +1,44 @@
-
 import LibraryDropDown from "../Components/LibraryDropDown";
 import { useState } from "react";
-import SideMenu from "./SideMenu";
 import { LibraryEntity } from '../Components/LibraryDropDown';
-
-
-
+import HomeIcon from '@mui/icons-material/Home';
+import SideMenu from "./SideMenu";
 
 function HomePage() {
-
   const [selectedLibrary, setSelectedLibrary] = useState<LibraryEntity | null>(null);
+  const [showDropDown, setShowDropDown] = useState<boolean>(true);
+
+  const handleHomeClick = () => {
+    setShowDropDown(true); // Mostrar o dropdown ao clicar no HomeIcon
+    setSelectedLibrary(null); // Limpar a biblioteca selecionada
+  };
+
+  const handleDropDownSelect = (library: LibraryEntity | null) => {
+    setSelectedLibrary(library);
+    setShowDropDown(false); // Esconder o dropdown ao selecionar uma biblioteca
+  };
 
   return (
     <div>
-
-      <LibraryDropDown
-        selectedLibrary={selectedLibrary} setSelectedLibrary={setSelectedLibrary} />
-        
-
-        <SideMenu selectedLibrary={selectedLibrary} />
-        
+    <div style={{ position: 'absolute', top: 0, left: 0, margin: '10px' }}>
+      <button onClick={handleHomeClick} style={{ backgroundColor: 'transparent', border: 'none', cursor: 'pointer' }}>
+        <HomeIcon style={{ fontSize: 40, color: 'black' }} />
+      </button>
     </div>
-  )
+    {showDropDown ? (
+      <div >
+        <LibraryDropDown
+          selectedLibrary={selectedLibrary}
+          setSelectedLibrary={handleDropDownSelect}
+        />
+      </div>
+    ) : (
+      <div>
+        <SideMenu selectedLibrary={selectedLibrary} />
+      </div>
+    )}
+  </div>
+);
 }
 
-export default HomePage
+export default HomePage;
